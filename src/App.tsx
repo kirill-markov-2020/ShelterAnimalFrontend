695ad0705549ab6535cbf5fca94ba0437475f29a
Кирилл Марков <kirill-markov-2020@bk.ru>
2025-05-15 18:07:53 +0300

    Настройка маршрутов

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import AnimalsPage from './pages/AnimalsPage';
+import ShelterInfoPage from './pages/ShelterInfoPage';
+import { CssBaseline, Container } from '@mui/material';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <CssBaseline />
+      <Navbar />
+      <Container sx={{ marginTop: 4 }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/animals" element={<AnimalsPage />} />
+          <Route path="/shelter-info" element={<ShelterInfoPage />} />
+        </Routes>
+      </Container>
+    </Router>
   );
-}
+};
 
 export default App;
